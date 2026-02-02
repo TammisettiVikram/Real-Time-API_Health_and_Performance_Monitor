0642730851582454279c91d0334d6e80e434d927
TammisettiVikram <vikramtammisetti@gmail.com>
2026-02-02 19:08:14 +0530

    Variables

--- backend/run.py
+++ backend/run.py
@@ -1,13 +1,6 @@
-from dotenv import load_dotenv
-import os
-
-# 1. Load variables before importing the app
-load_dotenv() 
-
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    # 2. Enable debug mode for a better dev experience
-    app.run(debug=True, port=5000)
+    app.run()
